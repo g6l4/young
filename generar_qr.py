d0426ee0e221c4dac149761d8ae8caf287c6364d
g6l4 <grupo6labo4@gmail.com>
2025-03-12 11:39:42 -0300

    Subir archivos33333

--- generar_qr.py
+++ generar_qr.py
@@ -1,7 +1,7 @@
 import qrcode
 
 # URL cruda del archivo HTML en GitHub
-pagina_url = "https://github.com/g6l4/young/blob/main/pagina.html"
+pagina_url = "https://g6l4.github.io/young/pagina.html"
 
 # Crear el código QR con la URL del archivo HTML
 qr = qrcode.make(pagina_url)
